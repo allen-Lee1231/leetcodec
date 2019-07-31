c98369916affb3def2a236f92ecf8e689530f4c0
allen <supermrli@hotmail.com>
2019-07-31 16:54:05 +0800

    made code more readable

--- add_two_numbers.c
+++ add_two_numbers.c
@@ -1,48 +1,49 @@
-///**
-// * Definition for singly-linked list.
-// * struct ListNode {
-// *     int val;
-// *     struct ListNode *next;
-// * };
-// */
-//
-//
-//
-//struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
-//    int carry = 0, node = 0;
-//    struct ListNode *N = malloc(sizeof(struct ListNode));
-//    struct ListNode *current = N;
-//    while (l1 != NULL || l2 != NULL){
-//        node = carry;
-//        if (l1 != NULL){
-//            node += l1->val;
-//        }
-//        if (l2 != NULL){
-//            node += l2->val;
-//        }
-//
-//        if (node >= 10){
-//            node -= 10;
-//            carry = 1;
-//        }
-//        else carry = 0;
-//
-//        current->val = node;
-//
-//        if (l1 != NULL) l1 = l1->next;
-//        if (l2 != NULL) l2 = l2->next;
-//        if (l1 || l2){
-//            current->next = malloc(sizeof(struct ListNode));
-//            current = current->next;
-//        }
-//        else if (carry){
-//            current->next = malloc(sizeof(struct ListNode));
-//            current = current->next;
-//            current->val = 1;
-//            break;
-//        }
-//
-//    }
-//    current->next = NULL;
-//    return N;
-//}
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     struct ListNode *next;
+* };
+*/
+
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    int carry = 0, node = 0;
+    struct ListNode *N = malloc(sizeof(struct ListNode));
+    struct ListNode *current = N;
+    while (l1 != NULL || l2 != NULL){
+        node = carry;
+        if (l1 != NULL){
+            node += l1->val;
+        }
+        if (l2 != NULL){
+            node += l2->val;
+        }
+
+        if (node >= 10){
+            node -= 10;
+            carry = 1;
+        }
+        else carry = 0;
+
+        current->val = node;
+
+        if (l1 != NULL) l1 = l1->next;
+        if (l2 != NULL) l2 = l2->next;
+        if (l1 || l2){
+            current->next = malloc(sizeof(struct ListNode));
+            current = current->next;
+        }
+        else if (carry){
+            current->next = malloc(sizeof(struct ListNode));
+            current = current->next;
+            current->val = 1;
+            break;
+        }
+
+    }
+    current->next = NULL;
+    return N;
+}
+
+
--- longest_string_no_repeating_character.c
+++ longest_string_no_repeating_character.c
@@ -1,59 +1,59 @@
-//#include <stdio.h>
-///*Without using Hashing map.
-//  Runtime: 8 ms, faster than 70.97% of C online submissions for Longest Substring Without Repeating Characters.
-//  Memory Usage: 7.8 MB, less than 12.12% of C online submissions for Longest Substring Without Repeating Characters.
-//  */
-//
-//
-//int lengthOfLongestSubstring(char * s){
-//    char* cur = s;
-//    int len = 0, max = 0, i = 0, l;
-//    while (*(cur++)!='\0') len++;
-//    if (!len) return 0;
-//    if (len == 1) return 1;
-//    if (len == 2){
-//        if (s[0] == s[1]){
-//            return 1;
-//        }
-//        else return 2;
-//    }
-//    //int container[3] = {0};
-//    int *container = calloc(len, sizeof(int));
-//    cur = s;
-//
-//    while (*s != '\0'){
-//        cur = s;
-//        l = 0;
-//        while (*++cur != '\0'){
-//            l++;
-//            if (*cur == *s){
-//                container[i] = l;
-//                break;
-//            }
-//        }
-//        if (*cur == '\0') container[i] = l+1;
-//        //printf("container %d: %d\n", i, container[i]);
-//        i++;
-//        s++;
-//    }
-//    for (i = len-2; i >= 0; i--){
-//        if (container[i] - container[i+1] > 1){
-//            container[i] = container[i+1] + 1;
-//        }
-//    }
-//
-//    for (i = 0; i < len; i++){
-//        if (max<container[i]) max = container[i];
-//    }
-//    free(container);
-//    return max;
-//}
-//
-//
-//int main()
-//{
-//    char* test="abcbda";
-//    printf("Longest length: %d\n", lengthOfLongestSubstring(test));
-//    getchar();
-//    return 0;
-//}
+#include <stdio.h>
+/*Without using Hashing map.
+  Runtime: 8 ms, faster than 70.97% of C online submissions for Longest Substring Without Repeating Characters.
+  Memory Usage: 7.8 MB, less than 12.12% of C online submissions for Longest Substring Without Repeating Characters.
+  */
+
+
+int lengthOfLongestSubstring(char * s){
+    char* cur = s;
+    int len = 0, max = 0, i = 0, l;
+    while (*(cur++)!='\0') len++;
+    if (!len) return 0;
+    if (len == 1) return 1;
+    if (len == 2){
+        if (s[0] == s[1]){
+            return 1;
+        }
+        else return 2;
+    }
+    //int container[3] = {0};
+    int *container = calloc(len, sizeof(int));
+    cur = s;
+
+    while (*s != '\0'){
+        cur = s;
+        l = 0;
+        while (*++cur != '\0'){
+            l++;
+            if (*cur == *s){
+                container[i] = l;
+                break;
+            }
+        }
+        if (*cur == '\0') container[i] = l+1;
+        //printf("container %d: %d\n", i, container[i]);
+        i++;
+        s++;
+    }
+    for (i = len-2; i >= 0; i--){
+        if (container[i] - container[i+1] > 1){
+            container[i] = container[i+1] + 1;
+        }
+    }
+
+    for (i = 0; i < len; i++){
+        if (max<container[i]) max = container[i];
+    }
+    free(container);
+    return max;
+}
+
+
+int main()
+{
+    char* test="abcbda";
+    printf("Longest length: %d\n", lengthOfLongestSubstring(test));
+    getchar();
+    return 0;
+}
